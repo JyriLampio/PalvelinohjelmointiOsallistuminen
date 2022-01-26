e81ef91f614251db09c0dd0b16330dc3104d8ed4
Jyri Lampio <jyri.lampio@gmail.com>
2022-01-26 10:45:34 +0200

    Osallistumistehtävä valmis

---
+++ src/main/java/Palvelinohjelmointi/demo/web/HelloController.java
@@ -0,0 +1,40 @@
+package Palvelinohjelmointi.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	@RequestMapping("alku")
+	@ResponseBody
+	public String sayHello() {
+		return "Hei";
+	}
+	
+	
+@RequestMapping("moi")
+@ResponseBody
+public String sayHelloToSomeone(@RequestParam(name="nimi", required=false, defaultValue = "Muumi") String etunimi, @RequestParam(name="ika") String age) {
+	return "Hei " + etunimi + ", " + age + " vuotta";
+	}
+
+@RequestMapping("/index")
+@ResponseBody
+public String returnIndexPage(){
+	return "This is the main page";
+	}
+
+@RequestMapping("/contact")
+@ResponseBody
+public String returnContacInfoe(){
+	return "This is the contact page";
+	}
+
+@RequestMapping("hello")
+@ResponseBody
+public String helloPage(@RequestParam(name="location", required=false, defaultValue = "World Citizen") String location, @RequestParam(name="name") String name){
+	return "Welcome to the " + location + " " + name;
+	}
+}
